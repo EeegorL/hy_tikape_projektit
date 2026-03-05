3cf570b4cfb2f08f10732952d5fe6b1258b3bda4
EeegorL <mikkolegezin@gmail.com>
2026-03-05 15:28:07 +0200

    pikku korjaus ja raportti

--- 2_tietokannan_suunnittelu/Categories.sql
+++ 2_tietokannan_suunnittelu/Categories.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Category(
+CREATE TABLE Categories(
     id INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
     /* description TEXT NOT NULL */ /* the category could have a description, but probably not necessary */
